ae4033c9b617588084c1ddeb20fdee06d73539a6
Shai James <skyandbeyond@gmail.com>
2018-04-22 22:24:46 +0300

    dawn3

--- contracts/invaders_dawn3/invaders.cpp
+++ contracts/invaders_dawn3/invaders.cpp
@@ -16,55 +16,74 @@ public:
     invaders( account_name self )
         :contract(self){}
 
+
+
     /***
      * Actions
      ****************************************************/
 
-    // @abi action
-    void scored( name username, uint64_t score, account_name identity_account, account_name application ){
+    void scored( string username, uint64_t score, account_name identity_account, account_name application ){
         eosio_assert(state_config::exists(), "Contract does not yet have an application key");
         eosio_assert(state_config::get().application == application, "Application key does not match");
         require_auth(identity_account);
         require_auth(application);
 
+
         TABLE_Scores _scores(_self, _self);
 
-        auto previousScore = _scores.find( username );
-        if(previousScore == _scores.end()) _scores.emplace( identity_account, [&]( auto& a ) {
+        hash index = usernameToHash(username);
+        auto record = _scores.find( index );
+        if(record == _scores.end()) _scores.emplace( identity_account, [&]( auto& a ) {
+            a.index = index;
             a.username = username;
             a.score = score;
             a.identity_account = identity_account;
         });
-        else _scores.modify( previousScore, 0, [&]( auto& a ) {
-            eosio_assert(previousScore->identity_account != identity_account, "User no longer has this account");
-            eosio_assert(previousScore->score < score, "Previous score was higher");
+        else _scores.modify( record, 0, [&]( auto& a ) {
+            eosio_assert(record->identity_account == identity_account, "User no longer has this account");
+            eosio_assert(record->score < score, "Previous score was higher");
             a.score = score;
         });
     }
 
-    // @abi action
     void own( account_name application ){
         require_auth(_self);
         require_auth(application);
-        eosio_assert(!state_config::exists(), "Application owner already exists");
-        state_config::set(config{application});
+        eosio_assert(!state_config::exists(_self), "Application owner already exists");
+        state_config::set(config{application}, _self);
+    }
+
+    void del( string username ){
+        require_auth(_self);
+
+        TABLE_Scores _scores(_self, _self);
+
+        auto itr = _scores.find( usernameToHash(username) );
+        eosio_assert(itr != _scores.end(), "No such username");
+        _scores.erase(itr);
     }
 
 
 private:
+    typedef uint64_t hash;
     /***
      * Structs
      ****************************************************/
 
     // @abi table scores i64
     struct scores {
-        name username;
+        hash index;
+        string username;
         uint64_t score;
         account_name identity_account;
 
-        name primary_key()const { return username; }
-        EOSLIB_SERIALIZE( scores, (username)(score)(identity_account) )
+        hash primary_key()const { return index; }
+        EOSLIB_SERIALIZE( scores, (index)(username)(score)(identity_account) )
     };
+
+    static hash usernameToHash(string username){
+        return std::hash<string>{}(username);
+    }
 
     struct config {
         account_name application;
@@ -81,4 +100,4 @@ private:
     typedef singleton<N(invaders), N(config), N(invaders), config>  state_config;
 };
 
-EOSIO_ABI( invaders, (scored)(own))
+EOSIO_ABI( invaders, (scored)(own)(del) )
